ca51f990b8531ab17ab3f81c235bea4eacbc8e08
donnyfauzi <donnyfauzi55@gmail.com>
2024-06-24 22:11:57 +0700

    update

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
-const express = require("express");
+const express = require("express")
 const app = express();
 const port = 3000;
+const c_beranda = require("./controller/c_beranda")
+const c_auth = require("./controller/c_auth")
 
-app.use(express.urlencoded({ extended: false }));
-app.set("view engine", "ejs")
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static("public"))
+app.set("view engine", "ejs");
 app.set("views", "./view-html")
 
+app.get("/", c_beranda.index)
+app.get("/login", c_auth.form_login)
 
+app.listen(port, () => {
+  console.log(`Aplikasi sudah siap, buka http://localhost:${port}`)
+});
