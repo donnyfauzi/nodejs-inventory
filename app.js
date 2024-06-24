1f122cdd04e42260b9690a44bc0e816cb5f29a7d
donnyfauzi <donnyfauzi55@gmail.com>
2024-06-24 20:28:41 +0700

    update

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("hello world")
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.urlencoded({ extended: false }));
+app.set("view engine", "ejs")
+app.set("views", "./view-html")
+
+
