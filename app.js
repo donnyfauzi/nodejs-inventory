8d68a60af35d32a1ec65151c908b28494a3b5611
donnyfauzi <donnyfauzi55@gmail.com>
2024-06-27 14:03:13 +0700

    update

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const express = require("express")
-const app = express();
-const port = 3000;
-const passport = require("passport")
-const cookieParser = require("cookie-parser")
-const session = require("express-session")
+const express       = require("express")
+const app           = express();
+const port          = 3000;
+const passport      = require("passport")
+const cookieParser  = require("cookie-parser")
+const session       = require("express-session")
 
 
-const c_beranda = require("./controller/c_beranda")
-const c_auth = require("./controller/c_auth")
+const c_beranda     = require("./controller/c_beranda")
+const c_auth        = require("./controller/c_auth")
 
-const cek_login = c_auth.cek_login
+const cek_login     = c_auth.cek_login
 
 app.use(cookieParser("secret"))
 app.use(session({
@@ -24,16 +24,15 @@ app.use(session({
 
 app.use(passport.initialize())
 app.use(passport.session())
-
 app.use(express.urlencoded({ extended: false }))
 app.use(express.static("public"))
+
 app.set("view engine", "ejs");
 app.set("views", "./view-html")
 
 app.get("/", c_beranda.index)
 app.get("/login", c_auth.form_login)
 app.post("/proses-login", c_auth.proses_login)
-
 app.get("/dashboard", cek_login,(req, res) => {
   res.send("<h1>halaman dashbord</h1>")
 })
